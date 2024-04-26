5be480e8f1067db39f38303d3b5db0704929a761
RealPacifist <the.real.a.pacifist@gmail.com>
2024-04-27 01:18:19 +0700

    UI for home, UI & Register is kinda finished

--- main/views.py
+++ main/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-def show_main(request):
-    context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
-    }
+
+def show_home(request):
+    context = {}
+
+    return render(request, "home.html", context)
+
+def show_login(request):
+    context = {}
 
     return render(request, "login.html", context)
+
+def show_register(request):
+    context = {}
+
+    return render(request, "register.html", context)
+
