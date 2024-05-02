e5fdd32f116a9aa8401f05b55a812c14afc819d1
RealPacifist <the.real.a.pacifist@gmail.com>
2024-05-02 21:41:30 +0700

    Added Universall Navbars

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ def show_main(request):
     context = {
     }
 
-    return render(request, "main.html", context)
+    return render(request, "home.html", context)
 
 def daftar_kontributor(request):
     context = {
