55d0e9e8a4f93d6834b572218a9e8ef6abd70b0a
Fikri Budianto <budf03@gmail.com>
2024-05-17 06:26:45 +0700

    add views for daftar_kontributor & langganan

--- main/views.py
+++ main/views.py
@@ -1,24 +1,150 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import psycopg2
+from django.db import connection as conn
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
+# fokus ke view langganan, halaman beli, cara ngurus querynya, belajar topik2 di pbp yg terkait
 
 def daftar_kontributor(request):
-    context = {
-    }
+    try:
+        cursor = conn.cursor()
+        query = """
+        (
+        SELECT 
+            c.nama, 
+            'Penulis Skenario' AS tipe, 
+            c.jenis_kelamin, 
+            c.kewarganegaraan
+        FROM 
+            contributors c
+        JOIN 
+            penulis_skenario ps ON c.id = ps.id
+        )
+        UNION
+        (
+        SELECT 
+            c.nama, 
+            'Pemain' AS tipe, 
+            c.jenis_kelamin, 
+            c.kewarganegaraan
+        FROM 
+            contributors c
+        JOIN 
+            pemain p ON c.id = p.id
+        )
+        UNION
+        (
+        SELECT 
+            c.nama, 
+            'Sutradara' AS tipe, 
+            c.jenis_kelamin, 
+            c.kewarganegaraan
+        FROM 
+            contributors c
+        JOIN 
+            sutradara s ON c.id = s.id;
+        )
+        """
+
+        cursor.execute(query)
+        all_rows = cursor.fetchall()
+
+        kontributor_list = []
+
+        for res in kontributor_list:
+            kontributor_list.append({
+                "id": res[0],
+                "nama": res[1],
+                "tipe": res[2],
+                "jenis_kelamin": res[3],
+                "kewarganegaraan": res[4]         
+            })
+
+        #send all data to html through context 
+        context = {
+            "kontributor_list" : kontributor_list,
+        }
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+
+    finally:
+        # closing database connection.
+        if conn:
+            cursor.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
 
     return render(request, "daftar_kontributor.html", context)
 
 def langganan(request):
-    context = {
-    }
+    try:
+        cursor = conn.cursor()
+        query = """
+
+        """
+
+        cursor.execute(query)
+        all_rows = cursor.fetchall()
+
+        kontributor_list = []
+
+        for res in kontributor_list:
+            kontributor_list.append({
+                "id": res[0],
+                "nama": res[1],
+                "tipe": res[2],
+                "jenis_kelamin": res[3],
+                "kewarganegaraan": res[4]         
+            })
+
+        cursor.execute()
+
+        #send all data to html through context 
+        context = {
+            'list_paket_langganan_aktif':list_paket_langganan_aktif,
+            'list_paket_lain':list_paket_lain,
+            'list_riwayat_transaksi':list_riwayat_transaksi
+        }
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
 
     return render(request, "langganan.html", context)
 
-def halaman_beli(request):
-    context = {
-    }
+@require_http_methods(["GET","POST"])
+def halaman_beli(request, beli):
+    if request.session.get('username') is not None:
+        try:
+            cursor = connection.cursor()
+            if request.method == "POST":
+                print()
+            else:
+                print()
+            connection.commit()
 
-    return render(request, "halaman_beli.html", context)
+        except (Exception, psycopg2.Error) as error:
+            print("Error while fetching data from PostgreSQL", error)
+
+        finally:
+            # closing database connection.
+            if connection:
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed")
+
+
+
+
+    return redirect('main:langganan')
 
 def show_home(request):
     context = {}
@@ -55,13 +181,140 @@ def show_trailer(request):
 
     return render(request, 'daftar_tayangan.html', context)
 
-def show_tayangan(request):
+def show_tayangan(request, id_tayangan):
+    tayangan_type = 'Unknown'
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM tayangan WHERE id = %s", [id_tayangan])
+        tayangan = cursor.fetchone()
+        cursor.execute("SELECT * FROM film WHERE id_tayangan = %s", [id_tayangan])
+        film = cursor.fetchone()
+        if film is not None:
+            tayangan_type = 'Film'
+        else:
+            cursor.execute("SELECT * FROM series WHERE id_tayangan = %s", [id_tayangan])
+            series = cursor.fetchone()
+            if series is not None:
+                tayangan_type = 'Series'
+
+    if tayangan_type == 'Unknown':
+        raise Http404("Tayangan does not exist")
     
+    total_view = conn.cursor()
+    if tayangan_type == 'Film':
+        total_view.execute("""
+            SELECT COUNT(*) as views
+            FROM (
+                SELECT 
+                    id_tayangan, 
+                    username, 
+                    EXTRACT(EPOCH FROM (end_date_time - start_date_time)) / 60 as watch_duration
+                FROM RIWAYAT_NONTON
+                WHERE id_tayangan = %s
+            ) as watch_sessions
+            JOIN film ON film.id_tayangan = watch_sessions.id_tayangan
+            WHERE watch_duration >= (film.durasi_film * 0.7)
+            GROUP BY watch_sessions.id_tayangan
+                        """, [id_tayangan])
+        total_view = total_view.fetchone()
+    # TODO Implement views for series
+    # elif tayangan_type == 'Series':
+
+    avg_rating = conn.cursor()
+    avg_rating.execute("""
+                SELECT AVG(rating) as avg_rating
+                FROM ULASAN
+                WHERE id_tayangan = %s
+                    """, [id_tayangan])
+    avg_rating = avg_rating.fetchone()
+
+    genre = conn.cursor()
+    genre.execute("""
+                SELECT genre
+                FROM GENRE_TAYANGAN
+                WHERE id_tayangan = %s
+                    """, [id_tayangan])
+    genre = genre.fetchall()
+    haveGenres = True
+    if not genre:
+        genre = "The genres have not been defined"
+        haveGenres = False
+    
+    actors = conn.cursor()
+    actors.execute("""
+                SELECT nama
+                FROM MEMAINKAN_TAYANGAN MT
+                JOIN PEMAIN P on MT.id_pemain = P.id
+                JOIN CONTRIBUTORS C on P.id = C.id
+                WHERE id_tayangan = %s
+                    """, [id_tayangan])
+    actors = actors.fetchall()
+    haveActors = True
+    if not actors:
+        actors = "The actors have not been defined"
+        haveActors = False
+
+    writers = conn.cursor()
+    writers.execute("""
+                SELECT nama
+                FROM MENULIS_SKENARIO_TAYANGAN MST
+                JOIN PENULIS_SKENARIO PS on MST.id_penulis_skenario = PS.id
+                JOIN CONTRIBUTORS C on PS.id = C.id
+                WHERE id_tayangan = %s
+                    """, [id_tayangan])
+    writers = writers.fetchall()
+    haveWriters = True
+    if not writers:
+        writers = "The writers have not been defined"
+        haveWriters = False
+
+    sutradara = conn.cursor()
+    sutradara.execute("""
+                SELECT nama
+                FROM SUTRADARA S
+                JOIN CONTRIBUTORS C on S.id = C.id
+                WHERE S.id = %s
+                    """, [tayangan[7]])
+    sutradara = sutradara.fetchone()
+    if sutradara is None:
+        sutradara = "The director has not been defined"
+    else:
+        sutradara = sutradara[0]
+                      
     context = {
         'user': request.user,
+        'tayangan_type': tayangan_type,
+        'id': id_tayangan,
+        'judul': tayangan[1],
+        'sinopsis': tayangan[2],
+        'asal_negara': tayangan[3],
+        'sinopsis_trailer': tayangan[4],
+        'url_video_trailer': tayangan[5],
+        'release_date_trailer': tayangan[6],
+        'id_sutradara': tayangan[7],
+        'sutradara': sutradara,
+        'total_view': total_view[0],
+        'avg_rating': round(avg_rating[0], 2),
+        'genre': genre,
+        'haveGenres': haveGenres,
+        'actors': actors,
+        'haveActors': haveActors,
+        'writers': writers,
+        'haveWriters': haveWriters
     }
-    
-    return render(request, 'halaman_tayangan.html', context)
+
+    if tayangan_type == 'Series':
+        return show_series(request, context)
+    elif tayangan_type == 'Film':
+        context['url_video_film'] = film[1]
+        context['release_date_film'] = film[2]
+        context['durasi_film'] = film[3]
+        return show_film(request, context)
+
+def show_film(request, context):
+    return render(request, 'halaman_film.html', context)
+
+def show_series(request, context):
+    return render(request, 'halaman_series.html', context)
 
 def show_episode(request):
     
