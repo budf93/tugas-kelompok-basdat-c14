6cb7f0a0112290d1e1c283a4df65fbcb2f0b50c2
Winoto Hasyim <winoto.hasyim@ui.ac.id>
2024-05-19 19:50:11 +0700

    Solved runserver error

--- tugas_kelompok_basdat_c14/settings.py
+++ tugas_kelompok_basdat_c14/settings.py
@@ -10,8 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-4@)**bc9a_ln0xe_i&k8k2*40he1==o_^9r-(=$_ba_43n0)k9'
 
@@ -66,7 +64,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'main.views.loggedin_user',
             ],
         },
     },
